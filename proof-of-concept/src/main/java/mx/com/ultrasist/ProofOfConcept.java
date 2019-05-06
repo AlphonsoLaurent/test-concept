a97236a337d36b54c6daa4ee054295b53209b56e
alphonsolaurent <alphonsolaurent@gmail.com>
2019-05-06 17:47:32 -0500

    Correr aplicacion

--- proof-of-concept/src/main/java/mx/com/ultrasist/ProofOfConcept.java
+++ proof-of-concept/src/main/java/mx/com/ultrasist/ProofOfConcept.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProofOfConcept {
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		SpringApplication.run(ProofOfConcept.class, args);
 	}
+	
 }
