07414b19fa8f8b3d263ed08f1d8abdf5a11a2b79
alphonsolaurent <alphonsolaurent@gmail.com>
2019-05-07 09:47:01 -0500

    Correccion 2

--- proof-of-concept/src/main/java/mx/com/ultrasist/proof_of_concept/App.java
+++ proof-of-concept/src/main/java/mx/com/ultrasist/proof_of_concept/App.java
@@ -2,12 +2,15 @@ package mx.com.ultrasist.proof_of_concept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 @SpringBootApplication
-public class ProofOfConcept {
-	
+public class App {
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		SpringApplication.run(ProofOfConcept.class, args);
+		SpringApplication.run(App.class, args);
 	}
-	
+
 }
